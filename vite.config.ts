79c894d6df216832aaedb11d2f93b3948d331dff
José Jaraba <jijaraba@hotmail.com>
2024-08-26 12:39:23 -0500

    [UPD] Fix name module in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'Squadmakers',
+            name: 'squadmakers-components',
             fileName: 'squadmakers-components',
         },
         rollupOptions: {
